1994a8c12e4749a7f7941f91843e4a65dfab696e
lrpan12 <lrpan@umich.edu>
2023-03-08 12:39:07 -0500

    update data files

---
+++ chatgpt_data.R
@@ -0,0 +1,20 @@
+library(RedditExtractoR)
+
+chatgpt_urls = find_thread_urls(keywords='chatgpt', period='day')
+
+# loop
+chatgpt_subreddit_posts = data.frame()
+for(subreddit in chatgpt_urls$subreddit[1:15]){
+  subreddit_posts = find_thread_urls(period='day', subreddit = subreddit)
+  chatgpt_subreddit_posts = rbind(chatgpt_subreddit_posts, subreddit_posts)
+}
+dim(chatgpt_subreddit_posts)
+table(chatgpt_subreddit_posts$subreddit)
+
+length(unique(chatgpt_subreddit_posts$timestamp))
+
+# save
+save(chatgpt_subreddit_posts, file="chatgpt_subreddit0307.RData")
+load("chatgpt_subreddit0307.RData")
+getwd()
+kk
